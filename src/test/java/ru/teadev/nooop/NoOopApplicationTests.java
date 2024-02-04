89bd91321512a5659190d4b653d355924664a818
Dmitry Kovalenko <kovalenko@teadev.ru>
2024-02-05 01:06:20 +0400

    initial commit

---
+++ src/test/java/ru/teadev/nooop/NoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teadev.nooop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
