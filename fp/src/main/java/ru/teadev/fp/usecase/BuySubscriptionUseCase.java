2b98ae34adfa219f5503853720d3d4d3ae3f98a4
Dmitry Kovalenko <kovalenko@teadev.ru>
2024-02-12 21:39:58 +0400

    Move main logic to usecase class (for fp module)

--- fp/src/main/java/ru/teadev/fp/usecase/BuySubscriptionUseCase.java
+++ fp/src/main/java/ru/teadev/fp/usecase/BuySubscriptionUseCase.java
@@ -1,23 +1,53 @@
 package ru.teadev.fp.usecase;
 
 import lombok.RequiredArgsConstructor;
+import ru.teadev.fp.entity.Client;
+import ru.teadev.fp.entity.Discount;
 import ru.teadev.fp.entity.Order;
+import ru.teadev.fp.entity.Price;
+import ru.teadev.fp.entity.Subscription;
 import ru.teadev.fp.exceptions.CantBuySubscriptionException;
 import ru.teadev.fp.exceptions.ClientNotExistException;
 import ru.teadev.fp.exceptions.NotEnoughMoneyException;
 import ru.teadev.fp.exceptions.PaymentException;
 import ru.teadev.fp.exceptions.SubscriptionNotAvailable;
-import ru.teadev.fp.service.OrderService;
+import ru.teadev.fp.service.ClientService;
+import ru.teadev.fp.service.PaymentService;
+import ru.teadev.fp.service.PromoService;
+import ru.teadev.fp.service.SubscriptionService;
 
 @RequiredArgsConstructor
 public class BuySubscriptionUseCase {
 
-    private final OrderService orderService;
+    private final ClientService clientService;
+    private final SubscriptionService subscriptionService;
+    private final PromoService promoService;
+    private final PaymentService paymentService;
 
     @SuppressWarnings("unused")
     public void execute(Order order) throws CantBuySubscriptionException {
         try {
-            orderService.acceptSubscription(order);
+            Client client = order.getClient();
+            if (!clientService.isClientExist(client)) {
+                throw new ClientNotExistException(client);
+            }
+
+            Subscription subscription = order.getSubscription();
+            if (!subscriptionService.isSubscriptionAvailable(subscription)) {
+                throw new SubscriptionNotAvailable(subscription);
+            }
+
+            Discount discount = promoService.calculateDiscount(client, subscription);
+            Price price = promoService.applyDiscount(subscription.getBasePrice(), discount);
+
+            if (!clientService.isEnoughMoney(client, price)) {
+                throw new NotEnoughMoneyException(client, price);
+            }
+
+            paymentService.makePayment(client, subscription, price);
+
+            subscriptionService.activateSubscription(client, subscription);
+
         } catch (ClientNotExistException | SubscriptionNotAvailable | NotEnoughMoneyException | PaymentException e) {
             throw new CantBuySubscriptionException(e);
         }
